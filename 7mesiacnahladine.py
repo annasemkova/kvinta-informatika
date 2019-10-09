1b9e27ee3e2dec286ef9d4c6ee25a41f3138a366
annasemkova <56343523+annasemkova@users.noreply.github.com>
2019-10-09 12:31:48 +0200

    Add files via upload

--- 7mesiacnahladine.py
+++ 7mesiacnahladine.py
@@ -5,20 +5,22 @@ import random
 
 #zadanie=michalpuskel.github.io
 
-def mesiac(x,y,a,b,v,v2):
+def mesiac(x,y,a,b,v):
+    #x,y=polohy prvého "hlavného" kruhu
     #a=farba mesiaca b=farba pozadia v=priemer jedného kruhu v2=vzdialenost oboch kruhov
+    v2=v/8*3
     canvas.create_oval(x,y,x+v,y+v,fill=a,outline=a)
     canvas.create_oval(x+v2,y,x+v2+v,y+v,fill=b,outline=b)
 
 def more():
     canvas.create_rectangle(0,400,850,850,fill="darkblue",outline="darkblue")
 
-def dvamesiace(v,v2):
+def dvamesiace(v):
     x1=random.randint(230,480)
     y2=random.randint(10,290)
     b=400-y2-v
-    mesiac(x1,y2,"yellow","white",v,v2)
-    mesiac(x1,400+b,"yellow","darkblue",v,v2)
+    mesiac(x1,y2,"yellow","white",v)
+    mesiac(x1,400+b,"yellow","darkblue",v)
 
 def vlajka(x,k):
     #k=farba vlajky
@@ -28,22 +30,32 @@ def vlajka(x,k):
 
 def zelenavlajka():
     vlajka(80,"darkgreen")
-    mesiac(130,175,"red","darkgreen",50,20)
+    mesiac(130,175,"red","darkgreen",50)
 
-def obratenymesiac(x,y,a,b,v,v2):
+def obratenymesiac(x,y,a,b,v):
+    v2=v-(v/8*3)
     canvas.create_oval(x,y,x+v,y+v,fill=a,outline=a)
     canvas.create_oval(x+v2-v,y,x+v2,y+v,fill=b,outline=b)
+
+def logo(x,y,v):
+    v2=v/8*3
+    h=v/8
+    #x,y=stred danej veci
+    mesiac(x+h,y,"lightblue","red",v)
+    obratenymesiac(x-v-h,y,"lightblue","red",v)
     
+def cervenavlajka():
+    vlajka(620,"red")
+    logo(695,180,40)
 
-
-def logo(x,y,a,b,v,v2):
-    mesiac(x,y,a,b,v,v)
-    obratenymesiac(x,y,a,b,v,v2)
+def lod(x,y,q):
+    canvas.create_rectangle(fill="brown",outline="brown")
+    #vlajka
+    canvas.create_polygon(fill="beige",outline="black")
+    #trup
+    canvas.create_polygon(x-q,y-q/2,x+q,y-q/2,x-q/2,y+q/2,x+q/2,y+q/2,fill="lightbrown",outline="black")
     
-
-vlajka(620,"red")
+cervenavlajka()
 zelenavlajka()
 more()
-dvamesiace(100,40)
-obratenymesiac(50,50,"red","pink",50,30)
-#logo(50,50,"yellow","pink",100,80)
+dvamesiace(100)
